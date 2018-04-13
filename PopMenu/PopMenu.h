a04f3e1e2c1dea79d2095cd77619e88e99058dde
Cali Castle <cali@calicastle.com>
2018-04-12 17:06:48 -0700

    Initial Commit

---
+++ PopMenu/PopMenu.h
@@ -0,0 +1,19 @@
+//
+//  PopMenu.h
+//  PopMenu
+//
+//  Created by Cali Castle on 4/12/18.
+//  Copyright © 2018 Cali Castle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopMenu.
+FOUNDATION_EXPORT double PopMenuVersionNumber;
+
+//! Project version string for PopMenu.
+FOUNDATION_EXPORT const unsigned char PopMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopMenu/PublicHeader.h>
+
+
