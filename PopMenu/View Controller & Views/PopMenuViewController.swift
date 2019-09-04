817173b2ff6e4f75957d45446731c22696b9a7f0
Oleksandr Kysil <gomer0513@gmail.com>
2019-09-04 14:49:26 +0300

    Supported swift 5 & Xcode 10.3.

--- PopMenu/View Controller & Views/PopMenuViewController.swift
+++ PopMenu/View Controller & Views/PopMenuViewController.swift
@@ -548,7 +548,7 @@ extension PopMenuViewController {
     
     /// When the menu action gets tapped.
     @objc fileprivate func menuDidTap(_ gesture: UITapGestureRecognizer) {
-        guard let attachedView = gesture.view, let index = actions.index(where: { $0.view.isEqual(attachedView) }) else { return }
+        guard let attachedView = gesture.view, let index = actions.firstIndex(where: { $0.view.isEqual(attachedView) }) else { return }
 
         actionDidSelect(at: index)
     }
@@ -601,7 +601,7 @@ extension PopMenuViewController {
         let touchLocation = gesture.location(in: actionsView)
         // Get associated index for touch location.
         if let touchedView = actionsView.arrangedSubviews.filter({ return $0.frame.contains(touchLocation) }).first,
-            let index = actionsView.arrangedSubviews.index(of: touchedView){
+            let index = actionsView.arrangedSubviews.firstIndex(of: touchedView){
             return index
         }
         
